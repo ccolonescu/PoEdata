23d6a91bd5ea3e83702fe57dc2ccc93b5a0e3aa9
Constantin Colonescu <ccolonescu@gmail.com>
2016-05-02 17:52:21 -0600

    PoEdata First
    This initializes PoEdata

---
+++ R/data_def.R
@@ -0,0 +1,73 @@
+#' andy
+#' @format Adata frame of hamburger franchises in 75 cities from Big Andy's Burger Barn.
+#' \describe{
+#'  \item{sales}{Monthly sales revenue ($1000s)}
+#'  \item{price}{A price index for all products sold in a given month}
+#'  \item{advert}{Expenditure on advertising ($1000s)}
+#'  }
+#' @source Courtesy of hamburger franchise, called Big Andy's Burger Barn
+"andy"
+
+#' bangla
+#' @format A data frame with 34 rows and 2 variables:
+#' \describe{
+#'   \item{p}{Price of sugar cane divided by the price of jute}
+#'   \item{a}{area planted in thousands of hectares in a region of Bangladesh.}
+#'}
+#' @source \url{http://www.principlesofeconometrics.com/poe4/poe4stata.htm}
+"bangla"
+
+#' beer
+#' @format 30 annual observations from a single household
+#' \describe{
+#'   \item{q}{litres of beer consumed}
+#'   \item{pb}{Price of beer ($)}
+#'   \item{pl}{price of other liquor ($)}
+#'   \item{pr}{price of remaining goods and services (an index)}
+#'   \item{i}{income ($)}
+#'}
+#' @source  \url{http://www.principlesofeconometrics.com/poe4/poe4stata.htm}
+"beer"
+
+#' bond
+#'@format data frame; obs: 102, monthly, (1968:1 - 1976.6)
+#'\describe{
+#'  \item{bond}{AA railroad yields}
+#'  }
+#'@Source:  Cryer (1986)
+"bond"
+
+#' br
+#' @format Obs:   1080 home sales in Baton Rouge, LA during mid-2005
+#' \describe{
+#'   \item{price}{sale price, dollars}
+#'   \item{sqft}{total square feet}
+#'   \item{bedrooms}{number of bedrooms}
+#'   \item{baths}{number of full baths}
+#'   \item{age}{age in years}
+#'   \item{Occupancy}{Owner = 1
+#'                    Vacant = 2
+#'                    Tenant = 3}
+#'   \item{Pool}{Yes = 1, No = 0}
+#'   \item{Style}{
+#'   Traditional = 1
+#'   Townhouse = 2
+#'   Ranch = 3
+#'   New Orleans = 4
+#'   Mobile Home = 5
+#'   Garden = 6
+#'   French = 7
+#'   Cottage = 8
+#'   Contemporary = 9
+#'   Colonial = 10
+#'   Acadian = 11
+#'   }
+#'   \item{Fireplace}{Yes = 1, No = 0}
+#'   \item{Waterfront}{Yes =1, No = 0}
+#'   \item{DOM}{Days on the market}
+#'}
+#' @source  Dr. Kelley Pace, Department of Finance,
+#'          Louisiana State University
+"br"
+
+
